6cac0cded197bb792a96d49f0c7b8e10f9a2da5d
dvtruong-0 <derektruong13@gmail.com>
2024-06-06 11:48:43 -0400

    added about me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profileImage from '../assets/me.png';
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <div classname="left">
-          <img classname="profile_image" ></img>
+        <div className="split left">
+          <div className="centered">
+            <img className="profile_image" src={profileImage} alt="Profile picture"></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Derek Truong</div>
+            <div className="brief_description">
+              <p>Hello! I am a current student at Brown University, pursuing a degree in Computer Engineering. I've been inspired by technology and innovation, hoping to work on quantum computers in the future.</p>
+              <p>A little more about me: </p>
+              <ul>
+                <li>I'm from Miami, FL.</li>
+                <li>My favorite music genres are rap and R&B.</li>
+                <li>I love to play basketball (GO HEAT!) and videogames.</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     )
